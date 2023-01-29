f004408734527e1df806c1379113ea3922499e09
Vieira14N <ianvieira0411@gmail.com>
2023-01-29 01:34:07 -0300

    Project created

---
+++ src/main/java/com/vieira14n/springproject/SpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.vieira14n.springproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectApplication.class, args);
+	}
+
+}
